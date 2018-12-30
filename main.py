346e7ec5e1fbb1341f366dd1d80cee5e075afc22
Tugrul Yatagan <tugrulyatagan@gmail.com>
2018-12-30 22:03:44 +0300

    balanced classifier decision tree

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 
 
 # All units are SI base units
-TOPOLOGY_RADIUS = 7000  # meters
-SIMULATION_DURATION = 36000  # seconds
+TOPOLOGY_RADIUS = 8000  # meters
+SIMULATION_DURATION = 72000  # seconds
 PACKET_RATE = 0.01  # per second
 PACKET_SIZE = 60  # bytes, header + payload, 13 + max(51 to 222)
-AVERAGING = 1
+AVERAGING = 5
 random.seed(42)  # for now seed is constant
 
 # NODE_NUMBER = 100
@@ -49,14 +49,14 @@ random.seed(42)  # for now seed is constant
 # # simulation.write_to_file('output/sim.txt')
 #
 # X_train, X_test, y_train, y_test = simulation.get_training_data()
-# classifier = DecisionTreeClassifier()
+# classifier = DecisionTreeClassifier(class_weight='balanced')
 # # classifier = DecisionTreeClassifier(criterion = "gini", random_state = 100,max_depth=3, min_samples_leaf=5)
 # # classifier = DecisionTreeClassifier(criterion = "entropy", random_state = 100, max_depth = 3, min_samples_leaf = 5)
 # # class_weight
 # classifier.fit(X_train, y_train)
 # y_pred = classifier.predict(X_test)
 # print(classification_report(y_test, y_pred))
-# print(confusion_matrix(y_test, y_pred, labels=[1,2,3]))
+# print(confusion_matrix(y_test, y_pred, labels=[1, 2, 3]))
 # print ("Accuracy is {:.3f} %".format(accuracy_score(y_test,y_pred)*100))
 #
 # simulation = Simulation(topology=topology, packet_rate=PACKET_RATE, packet_size=PACKET_SIZE, simulation_duration=SIMULATION_DURATION, sf=PacketSf.SF_Predictor, sfPredictor=classifier.predict)
@@ -69,8 +69,8 @@ random.seed(42)  # for now seed is constant
 
 
 
-node_number_list = range(50, 1001, 100)
-# markers = ['o', 'v', '^', '<', '>', 's', 'p', '*', 'D']
+node_number_list = range(50, 1001, 50)
+markers = ['o', 'v', '^', '<', '>', 's', 'p', '*', 'D']
 
 plt.figure()
 predictor_pdr_list = []
@@ -83,7 +83,7 @@ for node_number in node_number_list:
     simulation.run()
 
     X_train, X_test, y_train, y_test = simulation.get_training_data()
-    classifier = DecisionTreeClassifier()
+    classifier = DecisionTreeClassifier(class_weight='balanced')
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
 
@@ -107,106 +107,106 @@ plt.tight_layout()
 plt.savefig('output/predict_pdr.png', dpi=200)
 
 
-# plt.figure()
-# sf_list = [PacketSf.SF_7, PacketSf.SF_8, PacketSf.SF_9, PacketSf.SF_10, PacketSf.SF_11, PacketSf.SF_12, PacketSf.SF_Lowest, PacketSf.SF_Random]
-# for sf in sf_list:
-#     sys.stdout.write('\n{} '.format(sf))
-#     n_pdr_list = []
-#     for node_number in node_number_list:
-#         sys.stdout.write('.')
-#         pdr_averaging_sum = 0
-#         for repeat in range(AVERAGING):
-#             topology = Topology.create_random_topology(node_number=node_number, radius=TOPOLOGY_RADIUS)
-#             simulation = Simulation(topology=topology, packet_rate=PACKET_RATE, packet_size=PACKET_SIZE, simulation_duration=SIMULATION_DURATION, sf=sf)
-#             simulation_result = simulation.run()
-#             pdr_averaging_sum += simulation_result.pdr
-#         n_pdr_list.append(float(pdr_averaging_sum)/AVERAGING)
-#     plt.plot(node_number_list, n_pdr_list, label=sf.name)
-# plt.ylim(bottom=0)
-# plt.xlim([0, 1000])
-# plt.xlabel('Node Number')
-# plt.ylabel('PDR (%)')
-# # plt.title('topology_radius={}, packet_rate={}, gw_number=1'.format(TOPOLOGY_RADIUS, PACKET_RATE))
-# plt.grid(True)
-# plt.legend(loc='upper right', fontsize='small', title="SF", ncol=2)
-# plt.tight_layout()
-# plt.savefig('output/sf_pdr.png', dpi=200)
+plt.figure()
+sf_list = [PacketSf.SF_7, PacketSf.SF_8, PacketSf.SF_9, PacketSf.SF_10, PacketSf.SF_11, PacketSf.SF_12, PacketSf.SF_Lowest, PacketSf.SF_Random]
+for sf in sf_list:
+    sys.stdout.write('\n{} '.format(sf))
+    n_pdr_list = []
+    for node_number in node_number_list:
+        sys.stdout.write('.')
+        pdr_averaging_sum = 0
+        for repeat in range(AVERAGING):
+            topology = Topology.create_random_topology(node_number=node_number, radius=TOPOLOGY_RADIUS)
+            simulation = Simulation(topology=topology, packet_rate=PACKET_RATE, packet_size=PACKET_SIZE, simulation_duration=SIMULATION_DURATION, sf=sf)
+            simulation_result = simulation.run()
+            pdr_averaging_sum += simulation_result.pdr
+        n_pdr_list.append(float(pdr_averaging_sum)/AVERAGING)
+    plt.plot(node_number_list, n_pdr_list, label=sf.name)
+plt.ylim(bottom=0)
+plt.xlim([0, 1000])
+plt.xlabel('Node Number')
+plt.ylabel('PDR (%)')
+# plt.title('topology_radius={}, packet_rate={}, gw_number=1'.format(TOPOLOGY_RADIUS, PACKET_RATE))
+plt.grid(True)
+plt.legend(loc='upper right', fontsize='small', title="SF", ncol=2)
+plt.tight_layout()
+plt.savefig('output/sf_pdr.png', dpi=200)
 
 
-# plt.figure()
-# gw_number_list = range(1, 5)
-# for gw_number in gw_number_list:
-#     sys.stdout.write('\n{} '.format(gw_number))
-#     n_pdr_list = []
-#     for node_number in node_number_list:
-#         sys.stdout.write('.')
-#         pdr_averaging_sum = 0
-#         for repeat in range(AVERAGING):
-#             topology = Topology.create_random_topology(node_number=node_number, radius=TOPOLOGY_RADIUS, gw_number=gw_number)
-#             simulation = Simulation(topology=topology, packet_rate=PACKET_RATE, packet_size=PACKET_SIZE, simulation_duration=SIMULATION_DURATION, sf=PacketSf.SF_Lowest)
-#             simulation_result = simulation.run()
-#             pdr_averaging_sum += simulation_result.pdr
-#         n_pdr_list.append(float(pdr_averaging_sum)/AVERAGING)
-#     plt.plot(node_number_list, n_pdr_list, label=gw_number)
-# plt.ylim(bottom=0)
-# plt.xlim([0, 1000])
-# plt.xlabel('Node Number')
-# plt.ylabel('PDR (%)')
-# # plt.title('topology_radius={}, packet_rate={}, sf=lowest'.format(TOPOLOGY_RADIUS, PACKET_RATE))
-# plt.grid(True)
-# plt.legend(fontsize='small', title="GW Number")
-# plt.tight_layout()
-# plt.savefig('output/gw_pdr.png', dpi=200)
-#
-#
-# plt.figure()
-# radius_list = range(1000, 11001, 2000)
-# for radius in radius_list:
-#     sys.stdout.write('\n{} '.format(radius))
-#     n_pdr_list = []
-#     for node_number in node_number_list:
-#         sys.stdout.write('.')
-#         pdr_averaging_sum = 0
-#         for repeat in range(AVERAGING):
-#             topology = Topology.create_random_topology(node_number=node_number, radius=radius)
-#             simulation = Simulation(topology=topology, packet_rate=PACKET_RATE, packet_size=PACKET_SIZE, simulation_duration=SIMULATION_DURATION, sf=PacketSf.SF_Lowest)
-#             simulation_result = simulation.run()
-#             pdr_averaging_sum += simulation_result.pdr
-#         n_pdr_list.append(float(pdr_averaging_sum)/AVERAGING)
-#     plt.plot(node_number_list, n_pdr_list, label=radius)
-#
-# plt.ylim(bottom=0)
-# plt.xlim([0, 1000])
-# plt.xlabel('Node Number')
-# plt.ylabel('PDR (%)')
-# # plt.title('packet_rate={}, gw_number=1, sf=lowest'.format(PACKET_RATE))
-# plt.grid(True)
-# plt.legend(fontsize='small', title="Radius (m)")
-# plt.tight_layout()
-# plt.savefig('output/r_pdr.png', dpi=200)
-#
-#
-# plt.figure()
-# packet_rate_list = [0.005, 0.01, 0.02, 0.04, 0.08]
-# for packet_rate in packet_rate_list:
-#     sys.stdout.write('\n{} '.format(packet_rate))
-#     n_pdr_list = []
-#     for node_number in node_number_list:
-#         sys.stdout.write('.')
-#         pdr_averaging_sum = 0
-#         for repeat in range(AVERAGING):
-#             topology = Topology.create_random_topology(node_number=node_number, radius=TOPOLOGY_RADIUS)
-#             simulation = Simulation(topology=topology, packet_rate=packet_rate, packet_size=PACKET_SIZE, simulation_duration=SIMULATION_DURATION, sf=PacketSf.SF_Lowest)
-#             simulation_result = simulation.run()
-#             pdr_averaging_sum += simulation_result.pdr
-#         n_pdr_list.append(float(pdr_averaging_sum)/AVERAGING)
-#     plt.plot(node_number_list, n_pdr_list, label=packet_rate)
-# plt.ylim(bottom=0)
-# plt.xlim([0, 1000])
-# plt.xlabel('Node Number')
-# plt.ylabel('PDR (%)')
-# # plt.title('topology_radius={}, gw_number=1, sf=lowest'.format(TOPOLOGY_RADIUS))
-# plt.grid(True)
-# plt.legend(fontsize='small', title="Packet Rate (1/sec)")
-# plt.tight_layout()
-# plt.savefig('output/pr_pdr.png', dpi=200)
+plt.figure()
+gw_number_list = range(1, 5)
+for gw_number in gw_number_list:
+    sys.stdout.write('\n{} '.format(gw_number))
+    n_pdr_list = []
+    for node_number in node_number_list:
+        sys.stdout.write('.')
+        pdr_averaging_sum = 0
+        for repeat in range(AVERAGING):
+            topology = Topology.create_random_topology(node_number=node_number, radius=TOPOLOGY_RADIUS, gw_number=gw_number)
+            simulation = Simulation(topology=topology, packet_rate=PACKET_RATE, packet_size=PACKET_SIZE, simulation_duration=SIMULATION_DURATION, sf=PacketSf.SF_Lowest)
+            simulation_result = simulation.run()
+            pdr_averaging_sum += simulation_result.pdr
+        n_pdr_list.append(float(pdr_averaging_sum)/AVERAGING)
+    plt.plot(node_number_list, n_pdr_list, label=gw_number)
+plt.ylim(bottom=0)
+plt.xlim([0, 1000])
+plt.xlabel('Node Number')
+plt.ylabel('PDR (%)')
+# plt.title('topology_radius={}, packet_rate={}, sf=lowest'.format(TOPOLOGY_RADIUS, PACKET_RATE))
+plt.grid(True)
+plt.legend(fontsize='small', title="GW Number")
+plt.tight_layout()
+plt.savefig('output/gw_pdr.png', dpi=200)
+
+
+plt.figure()
+radius_list = range(1000, 11001, 2000)
+for radius in radius_list:
+    sys.stdout.write('\n{} '.format(radius))
+    n_pdr_list = []
+    for node_number in node_number_list:
+        sys.stdout.write('.')
+        pdr_averaging_sum = 0
+        for repeat in range(AVERAGING):
+            topology = Topology.create_random_topology(node_number=node_number, radius=radius)
+            simulation = Simulation(topology=topology, packet_rate=PACKET_RATE, packet_size=PACKET_SIZE, simulation_duration=SIMULATION_DURATION, sf=PacketSf.SF_Lowest)
+            simulation_result = simulation.run()
+            pdr_averaging_sum += simulation_result.pdr
+        n_pdr_list.append(float(pdr_averaging_sum)/AVERAGING)
+    plt.plot(node_number_list, n_pdr_list, label=radius)
+
+plt.ylim(bottom=0)
+plt.xlim([0, 1000])
+plt.xlabel('Node Number')
+plt.ylabel('PDR (%)')
+# plt.title('packet_rate={}, gw_number=1, sf=lowest'.format(PACKET_RATE))
+plt.grid(True)
+plt.legend(fontsize='small', title="Radius (m)")
+plt.tight_layout()
+plt.savefig('output/r_pdr.png', dpi=200)
+
+
+plt.figure()
+packet_rate_list = [0.005, 0.01, 0.02, 0.04, 0.08]
+for packet_rate in packet_rate_list:
+    sys.stdout.write('\n{} '.format(packet_rate))
+    n_pdr_list = []
+    for node_number in node_number_list:
+        sys.stdout.write('.')
+        pdr_averaging_sum = 0
+        for repeat in range(AVERAGING):
+            topology = Topology.create_random_topology(node_number=node_number, radius=TOPOLOGY_RADIUS)
+            simulation = Simulation(topology=topology, packet_rate=packet_rate, packet_size=PACKET_SIZE, simulation_duration=SIMULATION_DURATION, sf=PacketSf.SF_Lowest)
+            simulation_result = simulation.run()
+            pdr_averaging_sum += simulation_result.pdr
+        n_pdr_list.append(float(pdr_averaging_sum)/AVERAGING)
+    plt.plot(node_number_list, n_pdr_list, label=packet_rate)
+plt.ylim(bottom=0)
+plt.xlim([0, 1000])
+plt.xlabel('Node Number')
+plt.ylabel('PDR (%)')
+# plt.title('topology_radius={}, gw_number=1, sf=lowest'.format(TOPOLOGY_RADIUS))
+plt.grid(True)
+plt.legend(fontsize='small', title="Packet Rate (p/s)")
+plt.tight_layout()
+plt.savefig('output/pr_pdr.png', dpi=200)
